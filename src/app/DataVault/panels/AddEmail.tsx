5284aa06e6fadee18bc66c98b6e6acc3fef62642
vmandal <vmandal@gmail.com>
2021-05-03 15:41:44 +0530

    Email VC Issuied

--- src/app/DataVault/panels/AddEmail.tsx
+++ src/app/DataVault/panels/AddEmail.tsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { BaseButton } from '../../../components/Buttons'
 import Panel from '../../../components/Panel/Panel'
 import ServerConfig from '../../../config/config.server.json'
-import { createDidFormat, truncateAddressDid } from '../../../formatters'
-import { WebServiceInterface } from '../../../interfaces/webservice.interface'
+import { createDidFormat } from '../../../formatters'
+import { Web3ProviderContext } from '../../../providerContext'
+import DataVaultWebClient, { AuthManager, AsymmetricEncryptionManager, SignerEncryptionManager } from '@rsksmart/ipfs-cpinner-client'
 
 interface AddEmailInterface {
   address: string
   chainId: number
 }
-interface ResponseMessage {
-  message: string
-}
 
 const AddEmail: React.FC<AddEmailInterface> = ({ address, chainId }) => {
-  const [isError, setIsError] = useState<string | null>(null)
-
-  const [emailAddress, setEmailAddress] = useState('')
+  const [message, setMessage] = useState('')
+  const [email, setEmail] = useState('')
   const [emailCode, setEmailCode] = useState('')
   const [emailSent, setEmailSent] = useState(false)
   const [error, setError] = useState('')
-  const [headerStatus, setHeaderStatus] = useState(0)
+  const [jwt, setJwt] = useState('')
   const did = createDidFormat(address, chainId)
-
-  const [result, setResult] = useState<WebServiceInterface<ResponseMessage>>({ status: 'init' })
+  const context = useContext(Web3ProviderContext)
 
   const mailCode = () => {
-    setResult({ status: 'loading' })
+    setError('')
+    setMessage('')
+    console.log('calling: ', `${ServerConfig.backUrl}/issuer/mailCode/`)
+    let headerStatus = 0
     fetch(`${ServerConfig.backUrl}/issuer/mailCode/`, {
       method: 'POST',
       mode: 'cors',
@@ -37,33 +36,96 @@ const AddEmail: React.FC<AddEmailInterface> = ({ address, chainId }) => {
       },
       redirect: 'follow',
       referrerPolicy: 'no-referrer',
-      body: JSON.stringify({ emailAddress, did })
+      body: JSON.stringify({ email, did })
     }).then(response => {
-      console.log('response.status=', response.status)
-
-      if (!response.ok) {
-        response.json().then((jsonData) => {
-          console.log('jsonData=', jsonData)
-          setResult({ status: 'loaded', payload: jsonData })
-          if (result.status === 'loaded') throw new Error(result.payload.message)
-        })
-      } /* else if (!response.ok) {
-        throw new Error('HTTP status ' + response.status)
-      } */
-    })
+      headerStatus = response.status
+      return response
+    }).then(response => response.json())
+      .then(responseJson => {
+        if (headerStatus !== 200) {
+          throw new Error(responseJson.message)
+        } else {
+          setMessage(responseJson.message)
+          setEmailSent(true)
+        }
+      })
       .catch(handleError)
   }
 
   const handleError = (error: Error) => {
-    console.log('error in call')
     setError(error ? error.message : 'Unhandled error')
   }
 
   const verifyCode = () => {
-    // to do
+    setError('')
+    setMessage('')
+
+    const msg = `code:${emailCode}`
+    context.provider.request({
+      method: 'personal_sign',
+      params: [msg, address]
+    }).then((sig: string) => { issuerAddMail(msg, sig) })
+      .catch((error: any) => { setError(error.message) })
   }
 
-  const title = <>Add Email | did={did}</>
+  const issuerAddMail = (msg: string, sig: string) => {
+    setError('')
+    setMessage('')
+    console.log('calling: ', `${ServerConfig.backUrl}/issuer/AddMail/`, { code: emailCode, did, sig })
+
+    let headerStatus = 0
+    fetch(`${ServerConfig.backUrl}/issuer/AddMail/`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify({ did, msg, sig })
+    }).then(response => {
+      headerStatus = response.status
+      return response
+    }).then(response => response.json())
+      .then(responseJson => {
+        console.log('responseJson=', responseJson)
+        if (headerStatus !== 200) {
+          throw new Error(responseJson.message)
+        } else {
+          setMessage(responseJson.message)
+          setJwt(responseJson.jwt)
+        }
+      })
+      .catch(handleError)
+  }
+
+  const serviceUrl = 'https://data-vault.identity.rifos.org'
+
+  const getEncryptionManager = async (provider: any) => {
+    if (provider.isMetaMask && !provider.isNiftyWallet) return await AsymmetricEncryptionManager.fromWeb3Provider(provider)
+    return await SignerEncryptionManager.fromWeb3Provider(provider)
+  }
+
+  const saveInDataVault = () => getEncryptionManager(context.provider).then((encryptionManager) => new DataVaultWebClient({
+    authManager: new AuthManager({
+      did,
+      serviceUrl,
+      personalSign: (data: string) => context.provider!.request({ method: 'personal_sign', params: [data, address] })
+    }),
+    encryptionManager,
+    serviceUrl
+  }).create({ key: 'EmailVerifiableCredential', content: jwt })
+    .then((response) => {
+      console.log(response)
+      setMessage('Email Verifiable Credential saved')
+      setEmail('')
+      setJwt('')
+    })
+  ).catch(handleError)
+
+  const title = <>Add Email</>
 
   return (
     <Panel title={title} className="add-email">
@@ -71,7 +133,7 @@ const AddEmail: React.FC<AddEmailInterface> = ({ address, chainId }) => {
         <div className="column">
           <input type="text"
             className="line type"
-            onChange={(evt) => setEmailAddress(evt.target.value)}
+            onChange={(evt) => setEmail(evt.target.value)}
             disabled={emailSent}
             placeholder="Email" />
         </div>
@@ -81,25 +143,27 @@ const AddEmail: React.FC<AddEmailInterface> = ({ address, chainId }) => {
         <div className="column">
           <input type="text"
             className="line type"
-            onChange={(evt) => setEmailCode(evt.target.value)}
+            onChange={(evt) => { setEmailCode(evt.target.value); setJwt('') }}
             disabled={!emailSent}
             placeholder="Enter code" />
         </div>
         <div className="column submitColumn">
-          <BaseButton className="submit turquoise" onClick={verifyCode} disabled={!emailSent}>Verify</BaseButton>
+          <BaseButton className="submit turquoise" onClick={verifyCode} disabled={!emailSent || (jwt !== '') }>Verify</BaseButton>
+        </div>
+        <div className="column submitColumn">
+          <BaseButton className="submit turquoise" onClick={saveInDataVault} disabled={!jwt}>Save</BaseButton>
         </div>
       </div>
-      {isError && (
-        <div className="alert error">
-          <p>{isError}</p>
-        </div>
-      )}
       {error && (
         <div className="alert error">
           <p>{error}</p>
         </div>
       )}
-      | status = {result.status} | {result.status === 'loaded' && result.payload}
+      {message && (
+        <div className="alert info">
+          <p>{message}</p>
+        </div>
+      )}
     </Panel>
   )
 }
